a2bc63d1668105ec40e163356bcf620e29eb49eb
paoyako24 <paolaparedes248@gmail.com>
2026-04-10 13:01:26 -0600

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ItemsComponent } from './items/items.component';
+import { ItemsComponent } from './features/items/pages/items.component'; // <--- RUTA CLAVE
 
 export const routes: Routes = [
   { path: '', component: ItemsComponent }
