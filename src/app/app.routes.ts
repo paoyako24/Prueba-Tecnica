7e3077cdb347bbaa2de1f7c6cac170c616ddc312
paoyako24 <paolaparedes248@gmail.com>
2026-04-18 11:22:13 -0600

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
-import { ItemsComponent } from './features/items/pages/items.component'; // <--- RUTA CLAVE
+import { ItemsComponent } from './features/items/pages/items.component'; // Tu ejercicio 1
+import { FormularioReactivoComponent } from './formulario-reactivo/formulario-reactivo'; // Tu ejercicio 2
+import { ListadoProComponent } from './listado-pro/listado-pro';
 
 export const routes: Routes = [
-  { path: '', component: ItemsComponent }
+  { path: 'ejercicio1', component: ItemsComponent },
+  { path: 'ejercicio2', component: FormularioReactivoComponent },
+  { path: 'ejercicio3', component: ListadoProComponent }, // Cambié 'listado' por 'ejercicio3'
+  { path: '', redirectTo: 'ejercicio1', pathMatch: 'full' }
 ];
